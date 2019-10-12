f92f332bd41c4a3be699486994f38300af308cbf
louisewenjun <wenjuy2@uw.edu>
2019-10-12 11:46:35 -0700

    Solve multiprocessing issue
    This error message is displayed when using multiprocessing with the 'spawn' start method (default on platforms lacking fork like windows), and not protecting your code with a if __name__ = '__main__' guard.

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import argparse
 from RotEqCnn import RotEqCNN
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-d', '--dataset', type=str, default='mnist')
-parser.add_argument('--ensemble_num', type=int, default=9, help='Number of ensemble member')
-parser.add_argument('--train_epoch', type=int, default=6, help='Number of training epochs')
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dataset', type=str, default='mnist')
+    parser.add_argument('--ensemble_num', type=int, default=9, help='Number of ensemble member')
+    parser.add_argument('--train_epoch', type=int, default=6, help='Number of training epochs')
 
-opt = parser.parse_args()
-dataset = opt.dataset
-e_num = int(opt.ensemble_num)
-t_epoch = opt.train_epoch
+    opt = parser.parse_args()
+    dataset = opt.dataset
+    e_num = int(opt.ensemble_num)
+    t_epoch = opt.train_epoch
 
-recnn = RotEqCNN(e_num, dataset, t_epoch)
-recnn.get_dataset()
-recnn.init_models()
-recnn.train()
-recnn.show_test_result()
+    recnn = RotEqCNN(e_num, dataset, t_epoch)
+    recnn.get_dataset()
+    recnn.init_models()
+    recnn.train()
+    recnn.show_test_result()
+if __name__ == '__main__':
+    #freeze_support() #here if program needs to be frozen
+    main()
